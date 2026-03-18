996ee66d4254fdb1ae677058f31be07854440b87
Suleyman Cisse <suleyman.cisse@hva.nl>
2026-03-18 01:10:22 +0100

    Upload New File

---
+++ get_token.sh
@@ -0,0 +1,36 @@
+
+PAYLOAD="$(cat <<EOF
+{
+    "audience": "//iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
+    "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
+    "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
+    "scope": "https://www.googleapis.com/auth/cloud-platform",
+    "subjectTokenType": "urn:ietf:params:oauth:token-type:jwt",
+    "subjectToken": "${GITLAB_OIDC_TOKEN}"
+}
+EOF
+)"
+echo "PAYLOAD: " $PAYLOAD
+
+FEDERATED_TOKEN="$(curl -X POST "https://sts.googleapis.com/v1/token" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --data "${PAYLOAD}" \
+  | grep -Po '"access_token": *\K"[^"]*"'
+)"
+echo "FEDERATED_TOKEN: " $FEDERATED_TOKEN
+
+ACCESS_TOKEN="$(curl -X POST "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SERVICE_ACCOUNT_EMAIL}:generateAccessToken" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${FEDERATED_TOKEN}" \
+  --data '{"scope": ["https://www.googleapis.com/auth/cloud-platform"]}' \
+ | grep -Po '"accessToken": *\K"[^"]*"'
+)"
+
+echo "ACCESS_TOKEN: " $ACCESS_TOKEN
+
+echo $ACCESS_TOKEN> gcp_temp_token.txt
+
+
+
